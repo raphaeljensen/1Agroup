ca132acd43446862d948775776a659ba247f31e0
dgaleas <donaldoisthebest@gmail.com>
2017-06-05 14:43:33 -0700

    1

---
+++ ContactList.java
@@ -0,0 +1,53 @@
+/**
+ * This class will create, modify, print, store, and open a contact list
+ * including first name, Last name, street address,
+ * emain, phone number, and notes on contact
+ * @author Donaldo
+ *
+ */
+public class ContactList {
+private String firstname;
+private String lastname;
+private String streetaddress;
+private String email;
+private int number;
+private String notes;
+
+/**
+ * This Method will read contact info from user
+ * (Maybe create object?)
+ */
+public void Readinput(){//Create separate class
+	
+}//End of Readinput
+/**
+ * 
+ * @return
+ */
+public String   Showcontact(){
+	return "";
+}//End of Showcontact
+/**THis Method will return 
+ * all contacts that match the last name inputed 
+ * by user
+ * @return
+ */
+
+public String  Checkname() {
+	return "";
+}//End Checkname
+/**
+ * This method will save contact list to a file
+ * on hard disk
+ */
+public void save(){
+	
+}//End Save
+/**
+ * This method will open whatever file was saved when asked by user
+ */
+
+public void open(){
+	
+}//End open
+}//End of class
